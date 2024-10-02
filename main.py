d4988f1f3aafc734306c20873d9c4efe641174d8
Papouch9 <143577861+Papouch9@users.noreply.github.com>
2024-10-02 23:00:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ email: mira.kalik@seznam.cz
 discord: mira_47271
 '''
 
+
 import sys
 import requests
 import bs4
 import csv
 
+
 def nacti_jednotlive_obce(adresa: str) -> tuple:
+    """
+    Tato funkce uklada kody jednotlivych obci ve volebim okrsku.
+    :param adresa: Adresa webove stranky odkazujici na prehled volebniho okrsku.
+    :return: Funkce vraci tuple, ktery obshauje kod volebniho okrsku a kody jednotlivyh obci,
+            ktere se v okrsku nachazeji.
+    """
     seznam = []
+    # Overeni platnosti web adresy:
     try:
         html_soubor = requests.get(adresa)
     except:
         print("Nezadal jsi platnou adresu.\n"
               "Program bude ukončen.")
         exit()
+
+    # Ulozeni kodu obci a okrsku do vystupniho tuplu:
     kod_okresu = adresa[-4:]
     rozdeleny_html_soubor = bs4.BeautifulSoup(html_soubor.text, features="html.parser")
 
@@ -25,13 +36,21 @@ def nacti_jednotlive_obce(adresa: str) -> tuple:
         nactene_obce = rozdeleny_html_soubor.find_all("td", {"class": "cislo", "headers": f"t{tab}sa1 t{tab}sb1"})
     for no in nactene_obce:
         seznam.append(no.get_text())
-        #seznam[i] = (str(seznam[i])[87:93])
+
     return seznam, kod_okresu
 
 
 def nacti_vysledky_obci(seznam_obci: tuple) -> list:
+    """
+    Funkce vraci statistiky voleb v jednotlivych obcich ve vybranem volebnim okrsku.
+    :param seznam_obci: Tuple, ktery obsahuje kod volebniho okrsku a kody jednotlivyh obci,
+            ktere se v okrsku nachazeji.
+    :return: List dictu, ktery obsahuje hodnoty v jednotlivych obcich.
+    """
     okres = seznam_obci[1]
     celkova_statistika = []
+
+    # Vytvoreni 1. casti statistiky (souhrne informace o obci):
     for i in seznam_obci[0]:
         strany = []
         hlasy = []
@@ -48,6 +67,7 @@ def nacti_vysledky_obci(seznam_obci: tuple) -> list:
                            "envelopes": obalek.text.replace('\xa0', ''),
                            "valid": platne_hlasy.text.replace('\xa0', '')}
 
+        # Vytvoreni 2. casti statistiky (informace o jednotlivych stranach):
         for tab in range(1, 3):
             nactene_strany = rozdeleny_html_soubor.find_all("td", {"class": "overflow_name", "headers": f"t{tab}sa1 t{tab}sb2"})
             for ns in nactene_strany:
@@ -59,9 +79,17 @@ def nacti_vysledky_obci(seznam_obci: tuple) -> list:
         zaznamy = list(zip(strany, hlasy))
         statistika_obce.update(zaznamy)
         celkova_statistika.append(statistika_obce)
+
     return celkova_statistika
 
+
 def zapis_do_souboru(soubor: str, statistika: list) -> None:
+    """
+    Funkce uklada zjistene statistiky do csv souboru.
+    :param soubor: Uzivatelem zadane jmeno souboru.
+    :param statistika: Nalezene hodnoty v hledanem okrsku.
+    :return: None, pouze vytvari pozadovany csv soubor.
+    """
     with open(soubor, mode="w", encoding="UTF-8", newline='') as otevreny_soubor:
         zahlavi = statistika[0].keys()
         zapis = csv.DictWriter(otevreny_soubor, fieldnames=zahlavi)
